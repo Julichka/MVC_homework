0b7660989a273b95ee48010840dfd97b6ad81d6f
Yuliia Khrupina <julichka1391@gmail.com>
2022-02-27 09:52:37 -0800

    add table view

--- TodoListHomework2/ViewController.swift
+++ TodoListHomework2/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var add: UIButton!
+    @IBOutlet weak var edit: UIButton!
+    @IBOutlet weak var sort: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
