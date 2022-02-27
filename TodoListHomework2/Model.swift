2e5c38b81ae3c7d13cfcf138692be72270254993
Yuliia Khrupina <julichka1391@gmail.com>
2022-02-27 10:43:30 -0800

    add dialog + sort

---
+++ TodoListHomework2/Model.swift
@@ -0,0 +1,38 @@
+//
+//  Model.swift
+//  TodoListHomework2
+//
+//  Created by Yuliia Khrupina on 2/27/22.
+//
+
+import Foundation
+
+class TodoItem {
+    var name: String
+    var checked: Bool
+    
+    init(name: String, checked: Bool) {
+        self.name = name
+        self.checked = checked
+    }
+}
+
+class Model {
+    var todos = [
+        TodoItem(name: "item 1", checked: true),
+        TodoItem(name: "item 2", checked: false),
+        TodoItem(name: "item 3", checked: false)
+    ]
+    
+    func addItem(name: String, checked: Bool = false) {
+        todos.append(TodoItem(name: name, checked: checked))
+    }
+
+    func removeItem(at index: Int) {
+        todos.remove(at: index)
+    }
+    
+    func sort() {
+        todos = todos.sorted { $0.name < $1.name }
+    }
+}
--- TodoListHomework2/ViewController.swift
+++ TodoListHomework2/ViewController.swift
@@ -7,15 +7,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
     @IBOutlet weak var table: UITableView!
     @IBOutlet weak var add: UIButton!
     @IBOutlet weak var edit: UIButton!
     @IBOutlet weak var sort: UIButton!
     
+    var alert = UIAlertController()
+    
+    var model: Model = Model()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.register(UITableViewCell.self,
+                               forCellReuseIdentifier: "TableViewCell")
+        table.dataSource = self
+    }
+    
+    @IBAction func onAddClicked(_ sender: Any) {
+        alert = UIAlertController(title: "Create new task", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { (textField: UITextField) in
+            textField.placeholder = "Put your task here"
+            textField.addTarget(self, action: #selector(self.alertTextFieldDidChange(_:)), for: .editingChanged)
+        }
+        
+        let createAlertAction = UIAlertAction(title: "Create", style: .default) { (createAlert) in
+            // add new task
+            
+            guard let unwrTextFieldValue = self.alert.textFields?[0].text else {
+                return
+            }
+            
+            self.model.addItem(name: unwrTextFieldValue)
+            self.model.sort()
+            self.table.reloadData()
+            
+            
+        }
+        
+        let cancelAlertAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        alert.addAction(cancelAlertAction)
+        alert.addAction(createAlertAction)
+        present(alert, animated: true, completion: nil)
+        createAlertAction.isEnabled = false
+    }
+    
+    @objc func alertTextFieldDidChange(_ sender: UITextField) {
+
+            guard let senderText = sender.text, alert.actions.indices.contains(1) else {
+                return
+            }
+
+            let action = alert.actions[1]
+            action.isEnabled = senderText.count > 0
+        }
+    
+    @IBAction func onEdit(_ sender: Any) {
+        model.removeItem(at: 0)
+        self.table.reloadData()
+    }
+    
+    @IBAction func onSort(_ sender: Any) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.todos.count
+    }
+            
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "TableViewCell",
+                                                 for: indexPath)
+        cell.textLabel?.text = self.model.todos[indexPath.row].name
+        return cell
     }
 }
 
